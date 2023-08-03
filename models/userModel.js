71f411a4f1322de575ac2025443cf02c0190fe4c
harshpreet14 <kaurharshpt6@gmail.com>
2023-08-03 17:09:44 +0530

    Initial Commit

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  purchasedCourses:{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Course' }
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
